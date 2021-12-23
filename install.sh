a4e2e3d934b627b381e81b8efa524b4cd5ea7814
lifesboy <lifesboy@yahoo.com>
2021-12-23 08:06:43 +0700

    extract

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 mkdir -p ./bin
 wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz -o ./bin/gettext-0.21.tar.gz
-cd ./bin/gettext-0.21
+cd ./bin
+tar -zxvf gettext-0.21.tar.gz
+cd ./gettext-0.21
 ./configure
 make
 make install
