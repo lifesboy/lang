fc2609781061b4b0792f55653c9b9449b3d50e98
lifesboy <lifesboy@yahoo.com>
2021-12-23 08:24:42 +0700

    fix wget saving

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir -p ./bin
-wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz -o ./bin/gettext-0.21.tar.gz
+wget -v -O ./bin/gettext-0.21.tar.gz https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
 cd ./bin
 tar -zxvf gettext-0.21.tar.gz
 cd ./gettext-0.21
