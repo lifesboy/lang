9b607183a33caff8a98ce4a6718e083d34a4a96e
lifesboy <lifesboy@yahoo.com>
2021-12-25 17:53:43 +0700

    fix find core dir

--- make.sh
+++ make.sh
@@ -47,7 +47,7 @@ for LANG in ${LANGUAGES}; do
       if [ -d "${ROOTDIR}" ]; then
         echo ">>> Scanning ${ROOTDIR}";
         ${XGETTEXT_PL} -D "${ROOTDIR}" -p "${CURDIR}" -o "${TEMPLATE}.pot";
-        find "${ROOTDIR}" -type f -print0 | \
+        find "${ROOTDIR}/" -type f -print0 | \
             xargs -0 "${XGETTEXT}" -j -o "${CURDIR}/${TEMPLATE}.pot";
       fi
     done
