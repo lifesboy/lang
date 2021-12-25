aedd810f638c04cb59058c4b973cff8d0537d358
lifesboy <lifesboy@yahoo.com>
2021-12-25 23:01:55 +0700

    Add test file

---
+++ src/test.php
@@ -0,0 +1,19 @@
+<?php
+
+// Set language to German
+putenv('LC_ALL=vi_VN');
+setlocale(LC_ALL, 'vi_VN');
+
+// Specify location of translation tables
+// bindtextdomain("OPNsense", "./locale");
+
+// Choose domain
+textdomain("OPNsense");
+
+// Translation is looking for in ./locale/de_DE/LC_MESSAGES/myPHPApp.mo now
+
+require_once __DIR__."/acl.php";
+require_once __DIR__."/config.php";
+require_once __DIR__."/controllers.php";
+require_once __DIR__."/models.php";
+require_once __DIR__."/wizards.php";
