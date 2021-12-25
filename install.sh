3cf456c66ceedc97104cc1a85bd314bb75699c74
lifesboy <lifesboy@yahoo.com>
2021-12-25 17:33:55 +0700

    manual os when install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+os=$1
+
 mkdir -p ./bin
 wget -v -O ./bin/gettext-0.21.tar.gz https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
 cd ./bin
@@ -11,12 +13,12 @@ cd ../..
 apt-get install -y perl-doc
 perl -MCPAN -e "install Locale::Maketext::Lexicon"
 
-#BSD
-#PERL_DIR="/usr/local/lib/perl5/site_perl"
-#Linux
-#PERL_DIR="/usr/local/share/perl/5.28.1"
-#MacOS
-PERL_DIR=$PERL5LIB #"~/perl5/lib/perl5"
+#BSD, #Linux
+PERL_DIR="/usr/local/lib/perl5/site_perl"
+if [ "${os}" == "mac" ]; then
+  #MacOS
+  PERL_DIR=$PERL5LIB #"~/perl5/lib/perl5"
+fi
 PERL_NAME="Locale/Maketext/Extract/Plugin"
 
 mkdir -p "${PERL_DIR}/${PERL_NAME}/"
