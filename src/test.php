771b978d63b7076325660d2137c4d1a9e1157de8
lifesboy <lifesboy@yahoo.com>
2021-12-27 07:23:22 +0700

    refactor test file

--- src/test.php
+++ src/test.php
@@ -1,20 +1,23 @@
 <?php
 
-// Set language to German
-putenv('LC_ALL=vi_VN');
-setlocale(LC_ALL, 'vi_VN');
+// Set language to Vietnamese
+$locale = 'vi_VN';
+putenv("LC_ALL=$locale");
+setlocale(LC_ALL, $locale);
 
 // Specify location of translation tables
 // bindtextdomain("OPNsense", "./locale");
-bindtextdomain("OPNsense", __DIR__."../usr/share/locale");
+bindtextdomain("OPNsense", __DIR__."/../usr/share/locale");
 
 // Choose domain
 textdomain("OPNsense");
 
-// Translation is looking for in ./locale/de_DE/LC_MESSAGES/myPHPApp.mo now
+// Translation is looking for in ./locale/$locale/LC_MESSAGES/OPNsense.mo now
 
 require_once __DIR__."/acl.php";
 require_once __DIR__."/config.php";
 require_once __DIR__."/controllers.php";
 require_once __DIR__."/models.php";
 require_once __DIR__."/wizards.php";
+
+echo __DIR__."/../usr/share/locale/$locale/LC_MESSAGES/OPNsense.mo";
